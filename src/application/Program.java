08cc94dfe27f3ec98591a0ac2cf3c5c13d137938
Leonardo Paulino <proleonardopa@gmail.com>
2021-02-14 12:22:56 -0300

    Final implementation of class Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,7 +45,14 @@ public class Program {
 			worker.addContract(contract);
 		}
 		
-		
+		System.out.println();
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = kb.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));	
 		
 		kb.close();
 	}
